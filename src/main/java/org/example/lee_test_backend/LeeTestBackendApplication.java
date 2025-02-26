b05c851d9c1c646929ce733c68b313cc5452b392
leewoojin <zxsa999o@gmail.com>
2025-02-26 09:29:44 +0900

    [feat] dto 생성 및 엔티티 생성

---
+++ src/main/java/org/example/lee_test_backend/LeeTestBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.lee_test_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeTestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeeTestBackendApplication.class, args);
+	}
+
+}
